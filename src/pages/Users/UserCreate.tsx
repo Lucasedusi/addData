43aacf44c81eaadc8ab349c844eb134d46a5bf87
lucasedusi <lucasmto.16@hotmail.com>
2023-03-18 18:04:54 -0300

    feat:add stying in group page

--- src/pages/Users/UserCreate.tsx
+++ src/pages/Users/UserCreate.tsx
@@ -1,3 +1,38 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+import { useNavigate } from "react-router-dom";
+
+import { User } from "../../interfaces/User";
+import { Input } from "@chakra-ui/react";
+
 export function UserCreate() {
-	return <h1>Criação de Usuários</h1>;
+	const [users, setUsers] = useState<User[]>([]);
+
+	const { register, handleSubmit } = useForm<User>();
+
+	const navigate = useNavigate();
+
+	const addProducts: SubmitHandler<User> = async (data) => {
+		const newProduct: User = {
+			id: data.id + 1,
+			name: data.name,
+			email: data.email,
+		};
+
+		await axios
+			.post<User>("http://localhost:3000/users", newProduct)
+			.then(({ data }) => setUsers([...users, data]));
+
+		navigate("/");
+	};
+
+	return (
+		<form onSubmit={handleSubmit(addProducts)}>
+			<Input type="text" {...register("name")} />
+			<Input type="text" {...register("email")} />
+			<button type="submit">Add</button>
+		</form>
+	);
 }
--- src/pages/Users/UserList.tsx
+++ src/pages/Users/UserList.tsx
@@ -1,3 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { FaEdit } from "react-icons/fa";
+import axios from "axios";
+import { User } from "../../interfaces/User";
+
 export function UserList() {
-	return <h1>Lista de Usuários</h1>;
+	const [users, setUsers] = useState<User[]>([]);
+
+	useEffect(() => {
+		axios.get<User[]>("http://localhost:3000/users").then((response) => {
+			setUsers(response.data);
+		});
+	}, []);
+
+	return (
+		<div>
+			<Link to={"/create"}>Criar Usuário</Link>
+			<h1>Listagem de Usuários</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>Nome</th>
+						<th>E-mail</th>
+						<th>Ações</th>
+					</tr>
+				</thead>
+				<tbody>
+					{users.map((user) => (
+						<tr key={user.id}>
+							<td>{user.name}</td>
+							<td>{user.email}</td>
+							<td>
+								<Link to={`/users/${user.id}`}>
+									<FaEdit />
+								</Link>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
