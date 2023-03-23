171ceaafd2abb089ccd4157e43e75b038573aec1
lucasedusi <lucasmto.16@hotmail.com>
2023-03-23 10:26:47 -0300

    feat: add admin alert dialog

--- src/pages/Users/UserList.tsx
+++ src/pages/Users/UserList.tsx
@@ -3,6 +3,7 @@ import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import axios from "axios";
 import { User } from "../../interfaces/User";
+import { Box, Text } from "@chakra-ui/react";
 
 export function UserList() {
 	const [users, setUsers] = useState<User[]>([]);
@@ -30,36 +31,57 @@ export function UserList() {
 	}
 
 	return (
-		<div>
-			<Link to={"/create"}>Criar Usuário</Link>
-			<h1>Listagem de Usuários</h1>
-			<table>
-				<thead>
-					<tr>
-						<th>Nome</th>
-						<th>E-mail</th>
-						<th>Ações</th>
-					</tr>
-				</thead>
-				<tbody>
-					{users.map((user) => (
-						<tr key={user.id}>
-							<td>{user.name}</td>
-							<td>{user.email}</td>
-							<td>
-								<Link to={`/${user.id}/edit`}>
-									<FaEdit />
-								</Link>
-							</td>
-							<td>
-								<button type="button" onClick={() => handleRemoveUser(user.id)}>
-									<FaTrash />
-								</button>
-							</td>
+		<>
+			<Box
+				bgColor={"#fff"}
+				width={"300px"}
+				marginTop={"250px"}
+				marginLeft={"560px"}
+				padding={"20px"}
+			>
+				<Link to={"/create"}>Criar Usuário</Link>
+				<h1>Listagem de Usuários</h1>
+				<table>
+					<thead>
+						<tr>
+							<th>Nome</th>
+							<th>E-mail</th>
+							<th>Ações</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+					</thead>
+					<tbody>
+						{users.map((user) => (
+							<tr key={user.id}>
+								<td>{user.name}</td>
+								<td>{user.email}</td>
+								<td>
+									<Link to={`/${user.id}/edit`}>
+										<FaEdit />
+									</Link>
+								</td>
+								<td>
+									<button
+										type="button"
+										onClick={() => handleRemoveUser(user.id)}
+									>
+										<FaTrash />
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</Box>
+			<Text
+				bg={"#727CF5"}
+				padding={"10px"}
+				margin={"10px"}
+				color={"#fff"}
+				textAlign={"center"}
+				fontSize={"16px"}
+			>
+				Listagem de Usuários
+			</Text>
+		</>
 	);
 }
